ac8ade2e2243e9c2085e7773000277058081e750
chinmay768 <chinmaysahani07@gmail.com>
2025-05-17 22:28:19 +0530

    Produer

--- spring-kafka/producer/src/main/java/com/example/demo/KafkaProducer.java
+++ spring-kafka/producer/src/main/java/com/example/demo/KafkaProducer.java
@@ -1,4 +1,30 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class KafkaProducer {
+
+    // Instead of construction injection used autowire
+    @Autowired
+    private KafkaTemplate<String, RiderLocation> kafkaTemplate;
+
+
+    @PostMapping("/send")
+    public String sendMessage(@RequestParam String message){
+        RiderLocation riderLocation = new RiderLocation(323.00, 111.00, "ID12323");
+
+//        kafkaTemplate.send("my-new-topic", riderLocation); We can't send objects directly without serializing
+        kafkaTemplate.send("my-topic-new", riderLocation);
+        return "Message sent: " + riderLocation.getRiderId();
+    }
+
 }
+//spring.kafka.producer.key-serializer=org.apache.kafka.common.serialization.StringSerializer
+//spring.kafka.producer.value-serializer=org.springframework.kafka.support.serializer.JsonSerializer
--- spring-kafka/producer/src/main/java/com/example/demo/KafkaTopicConfig.java
+++ spring-kafka/producer/src/main/java/com/example/demo/KafkaTopicConfig.java
@@ -1,4 +1,16 @@
 package com.example.demo;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class KafkaTopicConfig {
+    @Bean
+    public NewTopic createMyTopic(){
+        return new NewTopic("my-new-topic", 3, (short) 1);
+    }
 }
+
+
+// You can create a new topic using spring boot also
